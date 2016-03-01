6f795f4744fb7f5bc127c729080fe642e0c3bbc6
shawn khah <shahram_khoshkhah@yahoo.com.au>
2016-03-01 18:22:58 -0500

    initial add of files

---
+++ Desktop/black rose garden/java script/scripts.js
@@ -0,0 +1,6 @@
+var visitor = prompt('What is your name?');
+var message = 'Hello  visitor ,  Welcome to black rose garden website.';
+message += 'We are so glad that you come by to visit.';
+message +=  visitor ;
+message += " ,plese come again, when you want to have beautiful garden.";
+document.write(message);
